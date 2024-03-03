50c8b1735bb7955969cd676d49f414342ec62805
Sravani520 <sravanichennupati222@gmail.com>
2024-03-03 14:03:15 +0530

    URL updated

--- appUtils.js
+++ appUtils.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 const fetchSensorDataFromAPI = async () => {
   try {
-    const response = await axios.get('http://localhost:5000/sensor-data');
+    const response = await axios.get('http://127.0.0.1:5000/sensor-data');
     return response.data.result; // Assuming the sensor data is nested under 'result'
   } catch (error) {
     console.error('Error fetching sensor data:', error);
